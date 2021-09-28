781fa5af5edc982903c0ff0f5b3698887d1bd93c
Denis <denisfurdilov@gmail.com>
2021-09-28 21:06:39 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Conway_s_Game_of_Life
+{
+    class Program
+    {
+        class Game
+        {
+            Game(int[] fieldSize)
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           
+        }
+
+    }
+}
