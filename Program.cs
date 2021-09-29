7fbb1714150fd895ac2fb47d7cb8b6b2cf40a2ab
hentaibaka <denisfurdilov@gmail.com>
2021-09-29 12:44:16 +0700

    Print method

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Conway_s_Game_of_Life
         {
             private uint _xSize;
             private uint _ySize;
-            private string _simv = "#";
+            private char _simv = '#';
             public Game() : this(new uint[2] { 32, 32 }) { }
             public Game(uint[] fieldSize)
             {
@@ -17,75 +17,86 @@ namespace Conway_s_Game_of_Life
                 _ySize = fieldSize[1];
             }
             public void Run()
-            {   
-                string[,] print      = new string[_ySize, _xSize];
-                string[,] printClone = new string[_ySize, _xSize];
-                string[,] printBufer = new string[_ySize, _xSize];
+            {
+                DateTime start;
+
+                bool[,] print      = new bool[_ySize, _xSize];
+                bool[,] printClone = new bool[_ySize, _xSize];
+                bool[,] printBufer = new bool[_ySize, _xSize];
+                bool[,] printZeros = new bool[_ySize, _xSize];
 
                 int neighboursCounter;
-                string bufer;
-                string output;
 
-                DateTime start;
-                
                 Random rnd = new Random();
                 
                 for (int i = 0; i < _ySize; i++) 
                 { 
                     for (int j = 0; j < _xSize; j++) 
                     {
-                        print[i, j] = rnd.Next(0, 3) == 2 ? _simv : " ";
-                        printBufer[i, j] = " ";
-                        printClone[i, j] = " ";
+                        print[i, j] = rnd.Next(0, 3) == 2;
+                        printBufer[i, j] = false;
+                        printClone[i, j] = false;
+                        printZeros[i, j] = false;
                     } 
                 }
                 do
                 {
-                    output = "";
+                    start = DateTime.Now;
+
                     for (int i = 1; i < _ySize - 1; i++)
                     {
                         for (int j = 1; j < _xSize - 1; j++)
                         {
-                            neighboursCounter = (print[i - 1, j - 1] == _simv ? 1 : 0)         // 1
-                                              + (print[i - 1, j    ] == _simv ? 1 : 0)         // 2
-                                              + (print[i - 1, j + 1] == _simv ? 1 : 0)         // 3    //1 2 3
-                                              + (print[i    , j - 1] == _simv ? 1 : 0)         // 4    //4 # 5
-                                              + (print[i    , j + 1] == _simv ? 1 : 0)         // 5    //6 7 8
-                                              + (print[i + 1, j - 1] == _simv ? 1 : 0)         // 6
-                                              + (print[i + 1, j    ] == _simv ? 1 : 0)         // 7
-                                              + (print[i + 1, j + 1] == _simv ? 1 : 0);        // 8
+                            neighboursCounter = (print[i - 1, j - 1] ? 1 : 0)         // 1
+                                              + (print[i - 1, j    ] ? 1 : 0)         // 2
+                                              + (print[i - 1, j + 1] ? 1 : 0)         // 3    //1 2 3
+                                              + (print[i    , j - 1] ? 1 : 0)         // 4    //4 # 5
+                                              + (print[i    , j + 1] ? 1 : 0)         // 5    //6 7 8
+                                              + (print[i + 1, j - 1] ? 1 : 0)         // 6
+                                              + (print[i + 1, j    ] ? 1 : 0)         // 7
+                                              + (print[i + 1, j + 1] ? 1 : 0);        // 8
+                            
 
                             // cell is 'live' and have 2 or 3 'live' neighbours
 
-                            if (print[i, j] == _simv && (neighboursCounter == 2 || neighboursCounter == 3)) bufer = _simv;
+                            if (print[i, j] && (neighboursCounter == 2 || neighboursCounter == 3)) printBufer[i, j] = true;
 
                             // cell is 'live' and have less then 2 or more then 3 neighbours
 
-                            else if (print[i, j] == _simv) bufer = " ";
+                            else if (print[i, j] && neighboursCounter != 2 && neighboursCounter != 3) printBufer[i, j] = false;
 
                             // cell is 'dead' and have 3 neighbours
 
-                            else if (print[i, j] == " " && neighboursCounter == 3) bufer = _simv;
-
-                            else bufer = " ";
-
-                            printBufer[i, j] = bufer;
-
-                            output += bufer;
+                            else if (!(print[i, j]) && neighboursCounter == 3) printBufer[i, j] = true;
                         }
-                        output += "\n";
                     }
-                    printClone = (string[,]) print.Clone();
-                    print = (string[,]) printBufer.Clone();
+                    printClone = (bool[,]) print     .Clone();
+                    print      = (bool[,]) printBufer.Clone();
+                    printBufer = (bool[,]) printZeros.Clone();
 
-                    Clear();
-                    Write(output);
-
-                    start = DateTime.Now;
-                    while ((DateTime.Now - start).Milliseconds < 100) continue;
+                    WriteLine((DateTime.Now - start).Milliseconds);
+                    PrintField(print);
+                    
                     //ReadKey();
                 }
                 while (true);
+            }
+            public void PrintField(bool[,] field)
+            {
+                DateTime start;
+                string output = "";
+                for (int i = 0; i < _ySize; i++)
+                {
+                    for (int j = 0; j < _xSize; j++)
+                    {
+                        output += field[i, j] ? _simv : ' ';
+                    }
+                    output += i == _ySize - 1 ? "" : "\n";
+                }
+                Clear();
+                Write(output);
+                start = DateTime.Now;
+                while ((DateTime.Now - start).Milliseconds < 70) continue;
             }
             public static void Rules()
             {
